5d467d6845da7eb960c65ab25a9cfd897fd7214e
Hieu <phambahieu2006@gmail.com>
2025-11-18 19:31:19 +0700

    create templete

---
+++ Src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My window");
+
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition({ 200, 200 });
+
+    while (window.isOpen()) {
+        
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.draw(circle);
+        window.display();
+    }
+}
